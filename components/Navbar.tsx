14d76cd2145b8be28afc16a07cbd15c1bb6ff72d
Kumar Sahil <2270168@kiit.ac.in>
2024-02-21 22:15:46 +0530

    sidebar, navbar

---
+++ components/Navbar.tsx
@@ -0,0 +1,38 @@
+"use client"
+import { cn } from "@/lib/utils"
+import { Menu, Sparkles } from "lucide-react"
+import Link from "next/link"
+import { Poppins } from "next/font/google"
+import { UserButton } from "@clerk/nextjs"
+import { Button } from "@/components/ui/button"
+import { ModeToggle } from "./mode-toggle"
+import { MobileSidebar } from "@/components/mobile-sidebar"
+
+const font = Poppins({
+    weight: "600",
+    subsets: ["latin"]
+})
+
+export const Navbar = () =>{
+    return(
+        <div className="fixed w-full z-50 flex justify-between items-center py-2 px-4 border-b border-primary/10 bg-secondary h-16">
+            <div className="flex items-center">
+            <MobileSidebar/>
+            <Link href="/">
+               <h1 className={cn("hidden md:block text-xl md:text-3xl font-bold text-primary"
+               ,font.className)}>
+                companion.ai
+                </h1> 
+            </Link>
+            </div>
+            <div className = "flex items-center gap-x-3">
+             <Button variant= "premium" size="sm">
+                Upgrade
+                <Sparkles className="h-5 w-5 fill-white text-white ml-2" />
+             </Button>
+             <ModeToggle/>
+             <UserButton/>
+            </div>
+        </div>
+    )
+}
