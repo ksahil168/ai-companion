a731d8ed58cb5dea65c3deafc64995a913c84ea0
Kumar Sahil <2270168@kiit.ac.in>
2024-02-22 09:54:43 +0530

    finished search 1:16:14

--- app/(root)/(routes)/page.tsx
+++ app/(root)/(routes)/page.tsx
@@ -1,9 +1,10 @@
+import { Searchinput } from "@/components/search-input";
 import { UserButton } from "@clerk/nextjs";
 
 const RootPage  = () => {
     return ( 
-        <div>
-            <UserButton afterSignOutUrl="/"/>
+        <div className="h-full p-4 space-y-4">
+           <Searchinput/>
         </div>
      );
  }
